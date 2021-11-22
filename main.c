5ab49468c7e72e9776a212b9cafe1e442d674dd3
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2021-11-22 22:07:30 +0100

    Ajouter Un tableau de 1000 case pour les structure avec un scope global, ajouter une variable nbrAccount pour boucle sur le tableau, ajouter une fonction addAccount pour ajouter n compte, ajouter une fonction des operations

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+
 
 //struct account
 struct compte {
-	int id;
 	char cin[10];
 	char nom[20];
 	char prenom[20];
 	float montant;
 };
 
+struct compte c[1000];
+int nbrAccount = 0;
 
 //function Clean
 void cleanCls(){
@@ -17,39 +20,116 @@ void cleanCls(){
 }
 
 //fonction
-bool addAccount(){
-	struct compte c;
-	printf("Entrer le CIN : ");
-	scanf("%s", c.cin);
+bool addAccount(int n){
+	//struct compte c[nbrAccount];
+	int i;
 	
-	printf("Entrer le nom : ");
-	scanf("%s", c.nom);
-	
-	printf("Entrer le prenom : ");
-	scanf("%s", c.prenom);
-	
-	printf("Entrer le montant : ");
-	scanf("%f", &c.montant);
-	
-	FILE *file;
-	file = fopen("./db.txt", "a");
-	fprintf(file, "%s %s %s %f \n", c.cin, c.nom, c.prenom, c.montant);
-	//fgets(data, 50, file);
-	//printf("%s", data);
-	char str1[20], str2[20], str3[20];
-	float montant;
-	fscanf(file, "%s %s %s %f", str1, str2, str3, &montant);
-	fclose(file);
-	printf("Montant : ", montant);
+	for (i = 0 ; i < n; i++){
+		printf("Entrer le CIN : ");
+		scanf("%s", c[i].cin);
+		
+		printf("Entrer le nom : ");
+		scanf("%s", c[i].nom);
+		
+		printf("Entrer le prenom : ");
+		scanf("%s", c[i].prenom);
+		
+		printf("Entrer le montant : ");
+		scanf("%f", &c[i].montant);
+		nbrAccount++;
+	}
 	return true;
 } 
-int main(){
-	int choix;
-	char data[50];
+
+//Depot
+bool depot(char cin[10]) {
 	
+}
+//fonction operation
+bool operations(){
+	int _choix, i;
+	char cin[10];
+	float operationValue;
+	
+	printf("Entrer le CIN du compte : ");
+	scanf("%s", cin);
+
+	cleanCls();
+	do {
+		printf("\n\n\n\n\n\n");
+		printf("\t \t \t 1. Depot \n \n \n");
+		printf("\t \t \t 2. Retrait \n \n \n");
+		printf("\t \t \t 00. Menu principale \n");
+		printf("Entrer un choix : ");
+		scanf("%d", &_choix);
+		
+		switch (_choix) {
+			case 1 : 
+				cleanCls();
+					for(i = 0; i < nbrAccount; i++) {
+						if (strcmp(cin, c[i].cin) == 0) {
+							printf("Entrer la valeur de depot : ");
+							scanf("%f", &operationValue);
+							c[i].montant += operationValue;
+							
+							printf("Le montant est : %g", c[i].montant);
+						}
+					}
+				break;
+			case 2 : 
+				cleanCls();
+				for(i = 0; i < nbrAccount; i++) {
+						if (strcmp(cin, c[i].cin) == 0) {
+							printf("Entrer la valeur de retrait : ");
+							scanf("%f", &operationValue);
+							if (c[i].montant >= operationValue)
+								c[i].montant -= operationValue;
+							else 
+								printf("Votre solde est inferieur à %g \n", operationValue);
+							
+							printf("Le montant est : %g \n \n \n", c[i].montant);
+						}
+					}
+				break;
+			default : printf("Votre choix n exist pas \n");
+		}
+	}while (_choix != 0);
+	cleanCls();
+}
+
+
+void affichage() {
+	cleanCls();
+	
+	int _choix;
+	int tri;
+	do {
+		printf("\t \t \t 1. tri par nom  \n \n \n");
+		printf("\t \t \t 2. tri par montant \n \n \n");
+		printf("\t \t \t 3. Recherche par CIN \n \n \n");
+		printf("\t \t \t 00. Menu principale \n \n \n");
+		printf("Entrer votre choix : ");
+		scanf("%d", &_choix);
+		
+		switch (_choix) {
+			case 1 : 
+				printf("\t \t \t 1. ASC \n \n \n");
+				printf("\t \t \t 2. DESC \n \n \n");
+				printf("Entrer votre choix : ");
+				scanf("%d", &_choix);
+				//sortingByName();
+				break;
+		}
+	}while (_choix != 0);
+}
+
+int main(){
+	int choix, nbrNewAccount, i;
+	char data[50], cin[10];
+
 
 	
-	
+	//Menu
 	do {
 		
 		printf("\n\n\n\n\n\n");
@@ -65,20 +145,23 @@ int main(){
 		
 		switch (choix) {
 			case 1 : 
-			cleanCls();
-				addAccount();
+				cleanCls();
+				if(addAccount(1)){
+					printf("Votre compte a ete cree avec succes ");
+				} else {
+					printf("Erreur !");
+				}
 				break;
-			case 2 : 
-				printf("Votre choix est 2");
+			case 2 :
+				printf("Entrer le nombre des comptes que vous pouvez cree : ");
+				scanf("%d", &nbrNewAccount);
+				addAccount(nbrNewAccount);
 				break;
-			case 3 : 
-				printf("Votre choix est 3");
+			case 3 :
+				operations();
 				break;
 			case 4 : 
-				printf("Votre choix est 4");
-				break;
-			case 5 : 
-				printf("Votre choix est 5");
+				affichage();
 				break;
 			case 6 : 
 				cleanCls();
